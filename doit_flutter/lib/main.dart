1487a9dca12ce8a37892e6cc09871588db8a59dc
gong <ddddy0114@gmail.com>
2023-03-27 22:48:59 +0900

    test

---
+++ doit_flutter/lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            
+          ),
+        ),
+      )
+    );
+  }
+}
