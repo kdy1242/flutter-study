3de418bdbaa7dc582edb412f52f4e64be2a98b88
gong <ddddy0114@gmail.com>
2023-03-30 13:58:45 +0900

    [ch04] 상수

---
+++ doit_flutter/lib/ch04/Constant.dart
@@ -0,0 +1,42 @@
+
+// const String d1;  // 오류
+//
+// class User {
+//   static const String d2; // 오류
+//
+//   void some() {
+//     const String d3;  // 오류
+//   }
+// }
+// const String d1 = 'String';
+//
+// class User {
+//   static const String d2 = 'hello';
+//
+//   void some() {
+//     const String d3 = 'hello';
+//
+//     d1 = 'world'; // 오류
+//     d2 = 'world'; // 오류
+//     d3 = 'world'; // 오류
+//   }
+// }
+
+// final int n1;
+//
+// class MyClass {
+//   final int n2;
+//   MyClass(this.n2);
+//
+//   void some() {
+//     final n3;
+//     n3 = 10;
+//     n3 = 20;
+//   }
+// }
+
+
+
+void main() {
+
+}
