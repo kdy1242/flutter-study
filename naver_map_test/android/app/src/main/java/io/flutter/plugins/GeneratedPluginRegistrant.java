66664c714915e40370b603a79daea8b83e2aeca2
gong <ddddy0114@gmail.com>
2023-07-01 23:01:14 +0900

    마커찍기

--- naver_map_test/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ naver_map_test/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -21,6 +21,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin flutter_naver_map, dev.note11.flutter_naver_map.flutter_naver_map.FlutterNaverMapPlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new com.baseflow.geolocator.GeolocatorPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin geolocator_android, com.baseflow.geolocator.GeolocatorPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.pathprovider.PathProviderPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin path_provider_android, io.flutter.plugins.pathprovider.PathProviderPlugin", e);
--- naver_map_test/lib/main.dart
+++ naver_map_test/lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() async {
   await _initialize();
@@ -19,15 +20,24 @@ Future<void> _initialize() async {
   );
 }
 
+// 현재 위치 가져오기
+Future<Position> getCurrentLocation() async {
+  Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
+
+  return position;
+}
+
 class NaverMapApp extends StatelessWidget {
   const NaverMapApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    late NaverMapController _mapController;
+    late NaverMapController mapController;
 
     // NaverMapController 객체의 비동기 작업 완료를 나타내는 Completer 생성
     final Completer<NaverMapController> mapControllerCompleter = Completer();
+
 
     return MaterialApp(
       home: Scaffold(
@@ -38,9 +48,36 @@ class NaverMapApp extends StatelessWidget {
             consumeSymbolTapEvents: false,  // 심볼 탭 이벤트 소비 여부 설정
           ),
           onMapReady: (controller) async {                // 지도 준비 완료 시 호출되는 콜백 함수
-            _mapController = controller;
+            mapController = controller;
             mapControllerCompleter.complete(controller);  // Completer에 지도 컨트롤러 완료 신호 전송
             log("onMapReady", name: "onMapReady");
+
+            //final locationOverlay = await mapController.getLocationOverlay();
+
+            // var currentLocation = await getCurrentLocation();
+
+            // log('${currentLocation.latitude}', name: 'currentLocation');
+
+            final marker = NMarker(
+                id: 'test',
+                position:
+                const NLatLng(37.506932467450326, 127.05578661133796));
+
+            final marker1 = NMarker(
+                id: 'test1',
+                position:
+                const NLatLng(37.606932467450326, 127.05578661133796));
+
+            controller.addOverlayAll({marker, marker1});
+
+            final onMarkerInfoWindow = NInfoWindow.onMarker(id: marker.info.id, text: '테스트');
+            marker.openInfoWindow(onMarkerInfoWindow);
+
+            onMarkerInfoWindow.setMinZoom(12);
+            onMarkerInfoWindow.setMaxZoom(14);
+            onMarkerInfoWindow.setIsMinZoomInclusive(true);
+            onMarkerInfoWindow.setIsMaxZoomInclusive(false);
+
           },
         ),
       ),
